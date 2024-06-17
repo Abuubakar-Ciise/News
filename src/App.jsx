f01a8956abf54c7e991b3eb185ab0cf2847790f2
Abuubakar Ciise <138715168+Abuubakar-Ciise@users.noreply.github.com>
2024-06-17 06:16:21 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import News from "./components/News";
 import LanguageDropdown from "./components/LanguageDropdown";
 
 const App = () => {
-  const [category, setCategory] = useState("general");
+  const [category, setCategory] = useState("");
   const [selectedLanguage, setSelectedLanguage] = useState("en");
 
   return (
