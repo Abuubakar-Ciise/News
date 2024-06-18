72d6e1ade83c15dc93c04e0ccd161dd53879b9cb
Abuubakar Ciise <138715168+Abuubakar-Ciise@users.noreply.github.com>
2024-06-18 10:52:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import Newsbar from "./components/Newsbar";
 import News from "./components/News";
 import LanguageDropdown from "./components/LanguageDropdown";
 
-const App = () => {
-  const [category, setCategory] = useState("");
-  const [selectedLanguage, setSelectedLanguage] = useState("en");
-
-  return (
+const App = ()=>{
+  const  [category,setCategory] = useState("")
+  const [selectedLanguage, setSelectedLanguage] = useState('');
+  return(
     <div>
-      <Newsbar setCategory={setCategory} />
-      <LanguageDropdown selectedLanguage={selectedLanguage} setSelectedLanguage={setSelectedLanguage} />
-      <News category={category} selectedLanguage={selectedLanguage} />
+    <Newsbar setCategory={setCategory}/>
+    <LanguageDropdown selectedLanguage={selectedLanguage} setSelectedLanguage={setSelectedLanguage} />
+    <News category={category} selectedLanguage={selectedLanguage}/>
+    
     </div>
-  );
-};
-
-export default App;
+  )
+}
+export default App
