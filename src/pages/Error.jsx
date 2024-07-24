66be90c43656d3a233a64a24241b737a59b15ad4
Abuubakar Ciise <138715168+Abuubakar-Ciise@users.noreply.github.com>
2024-07-24 14:34:37 +0300

    Create Error.jsx

---
+++ src/pages/Error.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import './Error.css'
+const Error = ({message})=> {
+
+    return(
+        <div className="container">
+            <div className="error">
+                <h1>Error</h1>
+                <p>{message}</p>
+            </div>
+        </div>
+    )
+}
+export default Error
