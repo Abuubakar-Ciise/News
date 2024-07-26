89673d1fb109e9397545ba4f587e02aa7fb57869
Abuubakar Ciise <138715168+Abuubakar-Ciise@users.noreply.github.com>
2024-07-26 16:27:30 +0300

    Update Error.jsx

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,13 @@
-
-import React from "react";
+import React, { useEffect } from "react";
 import './Error.css'
-const Error = ({message})=> {
 
-    return(
+const Error = ({ message }) => {
+
+    useEffect(() => {
+        alert("The API is unable to display news while the project is hosted online. Please download the project to view the news content. Thank you.");
+    }, []);
+
+    return (
         <div className="container">
             <div className="error">
                 <h1>Error</h1>
@@ -12,4 +16,5 @@ const Error = ({message})=> {
         </div>
     )
 }
-export default Error
+
+export default Error;
